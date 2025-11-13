88add550c72c5fb1027a54255593ae37acb8a4e2
Newfis <newfis24310@gmail.com>
2025-11-13 19:13:09 +0200

    Πρώτα βήματα

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+// Controls player movement and rotation.
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f; // Set player's movement speed.
+    private Rigidbody rb; // Reference to player's Rigidbody.
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // Access player's Rigidbody.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Handle physics-based movement and rotation.
+    private void FixedUpdate()
+    {
+        // Move player based on vertical input.
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = transform.forward * moveVertical * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+
+        // Rotate player based on horizontal input.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = transform.right * moveHorizontal * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+    }
+}
