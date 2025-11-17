4cd9682b6bdec036b699aab2bb9fb258b651f5ab
Georgia Sfakianaki <sfa.georgia@gmail.com>
2025-11-17 11:55:20 +0200

    UI HUD Menu: hearts now are made DYNAMICALLY through code and have icons

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,31 +1,45 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
-    public int lives = 3;
-    public HUDMenuUIController hudMenuController;
+    // Fields presented in the Inspector
+    public static int maxLives = 6;
+    public int currentLives = maxLives;
+    public HUDMenuUIController hudMenuController; // handles functionality in the UI
     public TextMeshProUGUI livesText;   // text object used to display lives
+    public Transform heartsContainer;
+    public GameObject livesIcon; // heartIcon prefab
+    public Sprite heartFull; // presentation of a full heart
+    public Sprite heartEmpty; // presentation of an empty heart
 
     private void Start()
     {
-        // Set the TMP reference INSIDE the controller
+        // Initialize dependencies with HUD Menu Controller
+        // we use methods for encapsulation - we don't directly open the field => fileds private
+        // we access them through methods
         hudMenuController.SetLivesText(livesText);
+        hudMenuController.SetHeartsContainer(heartsContainer);
+        hudMenuController.SetHeartIcon(livesIcon);
+        hudMenuController.SetHeartSprites(heartFull, heartEmpty);
 
-        // Show initial lives
-        hudMenuController.UpdateLives(lives);
+        // Build and show initial lives
+        hudMenuController.BuildHearts(maxLives);
+        hudMenuController.UpdateLives(currentLives, maxLives);
     }
 
     public void LoseOneLife()
     {
-        lives -= 1;
-        if (lives < 0) lives = 0;
+        currentLives -= 1;
+        if (currentLives < 0) currentLives = 0;
 
-        Debug.Log("Lives left: " + lives);
+        Debug.Log("Lives left: " + currentLives + "/" + maxLives);
 
-        hudMenuController.UpdateLives(lives);
+        hudMenuController.UpdateLives(currentLives, maxLives);
 
-        if (lives <= 0)
+        if (maxLives <= 0)
             EndGame();
     }
 
--- Assets/Scripts/HUDMenuUIController.cs
+++ Assets/Scripts/HUDMenuUIController.cs
@@ -1,25 +1,99 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class HUDMenuUIController : MonoBehaviour
 {
     private TextMeshProUGUI livesText;   // stays fully private
+    private Transform heartsContainer; // required to know where we will append the hearts
+    private GameObject heartIcon; // the element we want to append on the screen dynamiucally
+    private Sprite heartFull; // presentation of heart when the life is full
+    private Sprite heartEmpty; // presentation when life is lost
+    private List<Image> livesState = new List<Image>();
 
-    // GameManager (or whoever) will call this ONCE at initialization
+    // ---------------------------- SETTERS ---------------------------------------
+    // for the private fieds, so we can access them from other classes
     public void SetLivesText(TextMeshProUGUI text)
     {
         livesText = text;
     }
 
-    public void UpdateLives(int lives)
+    public void SetHeartsContainer(Transform container)
+    {
+        heartsContainer = container;
+    }
+
+    public void SetHeartSprites(Sprite heartFullIcon, Sprite heartEmptyIcon)
+    {
+        heartFull = heartFullIcon;
+        heartEmpty = heartEmptyIcon;
+    }
+
+    public void SetHeartIcon(GameObject heartIconPrefab)
+    {
+        heartIcon = heartIconPrefab;
+    }
+
+
+    //--------------------- BUILDING / UPDATES HEARTS -----------------------------------
+
+    // Method used to update the lives in the UI - presentation lvl
+    public void UpdateLives(int currentLives, int maxLives)
     {
         if (livesText != null)
         {
-            livesText.text = $"Lives: {lives}";
+            livesText.text = $"Lives: {currentLives}/{maxLives}";
         }
         else
         {
             Debug.LogWarning("HUDMenuUIController: livesText is NOT assigned!");
+        }
+        if (heartsContainer != null && livesState.Count != 0)
+        {
+            for (int life = 0; life < livesState.Count; life++)
+            {
+                livesState[life].sprite = life < currentLives ? heartFull : heartEmpty;
+            }
+        }
+        else
+        {
+            Debug.LogWarning("HUDMenuUIController: livesText is NOT assigned!");
+        }
+    }
+
+    // Method used to build the hearts in the UI dynamically. 
+    // why ? -> because we want if we set a different number to the game manager: maxLives = 6
+    // to have the same number of icons presented 
+    // used only once, then we just update the values ! Destroying and Rebuilding stuff directly
+    // on the hierarchy tree is not a good practice - it's expensive ! so we do this only once ! adding elements dynamically only once !
+    public void BuildHearts(int maxLives)
+    {
+        if (heartsContainer != null && heartIcon != null)
+        {
+            // create 1 heart per life
+            for (int life = 0; life < maxLives; life++)
+            {
+                // create a game object inside the heartsContainer
+                // we don't need to set the image - the prefab already knows the image
+                GameObject heartGO = Object.Instantiate(heartIcon, heartsContainer);
+                // get the image from the obj
+                // why ? -> we need to update our code state of lives
+                Image sprite = heartGO.GetComponent<Image>();
+                if (sprite != null)
+                {
+                    // add it to the list
+                    livesState.Add(sprite);
+                }
+                else
+                {
+                    Debug.LogWarning("Heart prefab has no Image component!");
+                }
+            }
+        }
+        else
+        {
+            Debug.LogWarning("HUDMenuController: heartsContainer or heartIcon NOT set");
         }
     }
 }
