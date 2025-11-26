f8fcab6626a7e4f61cb17a8707a707e824f4f7f6
Georgia Sfakianaki <sfa.georgia@gmail.com>
2025-11-26 13:46:50 +0200

    UI: Victory Menu + Letter: templates scaled up + fonts fixed to be more easily readable  + user-friendly + fontsize

--- Assets/Scripts/VictoryUI.cs
+++ Assets/Scripts/VictoryUI.cs
@@ -2,18 +2,20 @@ using UnityEngine;
 
 public class VictoryUI : MonoBehaviour
 {
-public GameObject notificationPanel;
-public GameObject readLetterPanel;
-public void OpenLetter()
+    public GameObject notificationPanel;
+    public GameObject readLetterPanel;
+    public GameObject backgroundOverlay;
+    public void OpenLetter()
     {
         notificationPanel.SetActive(false); // Κρύβει το αρχικό μήνυμα
         readLetterPanel.SetActive(true);    // Εμφανίζει το γράμμα
     }
 
-//Άμα θέλουμε να βάλουμε back, μπορούμε να χρησιμοποιήσουμε την παρακάτω method
-public void CloseLetter()
+    //Άμα θέλουμε να βάλουμε back, μπορούμε να χρησιμοποιήσουμε την παρακάτω method
+    public void CloseLetter()
     {
         readLetterPanel.SetActive(false);
-        notificationPanel.SetActive(true);
+        notificationPanel.SetActive(false);
+        backgroundOverlay.SetActive(false);
     }
 }
